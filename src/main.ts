39a3c443448945ab55ca00e3b2d4b8bb897b2f2e
Zainal Arifin <arifin.1602@gmail.com>
2021-01-09 19:49:29 +0700

    first init

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Shift Management')
+    .setDescription('Shift Management API Docs')
+    .setVersion('1.0')
+    .addTag('Shift')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
